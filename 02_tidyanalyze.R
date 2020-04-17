33158ebe450208062aa326393e5cf5eb522cff59
Anthony Savagar <asavagar@users.noreply.github.com>
2020-04-17 11:01:16 +0100

    new plot

--- 02_tidyanalyze.R
+++ 02_tidyanalyze.R
@@ -1,18 +1,13 @@
 require(tidyverse)
 require(lubridate)
-# df <- df %>% mutate(date = dmy(IncorporationDate))
 
 df <- read_csv("incorp_date.csv")
 
 df$IncorporationDate <- as.Date(df$IncorporationDate, "%d/%m/%Y")
 
 df$days <- as.numeric(df$IncorporationDate)
-# The base date is 1970-01-01
-# basedate <- df[ which(df$days >= 0), ]
 
-# 20 Jan 2020 is 18281
-
-newdata <- df[which(df$days >= 18280),]
+newdata <- df[which(df$days >= 18262), ]
 
 n_incorp <- newdata %>%
   group_by(IncorporationDate) %>%
@@ -30,12 +25,20 @@ full_period <- full_join(total_days, n_incorp, by = "day")
 
 full_period <- full_period %>% mutate(ni = replace_na(n.y, 0))
 
-df <- tibble(date = full_period$day, ni = full_period$ni)
+full_period <- tibble(date = full_period$day, ni = full_period$ni)
 
-p <- ggplot(data = df, aes(x = date, y = ni)) +
-  geom_bar(stat = "identity") + ggtitle("UK Daily Company Incorporations") +
-  xlab("20-01-20 to 31-03-20 (72 days)") + ylab("Number of Incorporations")
+positive <- full_period[which(full_period$ni > 0), ]
+
+med_incorp <- median(positive$ni)
+
+p <- ggplot(data = full_period, aes(x = date, y = ni)) +
+  geom_bar(stat = "identity") +
+  geom_hline(aes(yintercept = med_incorp, color = "Median")) +
+  labs(title = "UK Daily Company Incorporations",
+     x = "1-01-20 to 31-03-20",
+     y = "Number of Incorporations",
+     color = NULL)
 p
 
 ggsave("new_incorp_barplot.pdf")
-
+ggsave("new_incorp_barplot.png")
